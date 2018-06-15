5d13ccf34322f474f055addf5209d8912d910eb9
Roger Zhang <zhangqinyuan2013@live.cn>
2018-06-15 15:56:44 +0800

    Initialize Accordion Modern Web Part

---
+++ src/webparts/catDsgSpWp1006Accordion/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ICatDsgSpWp1006AccordionWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'CatDsgSpWp1006AccordionWebPartStrings' {
+  const strings: ICatDsgSpWp1006AccordionWebPartStrings;
+  export = strings;
+}
